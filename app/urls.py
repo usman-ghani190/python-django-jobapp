8c8f7a25a3e52a664c146759f77ba8aeaaafcf10
Usman <Cap.usman21@gmail.com>
2024-10-11 23:05:50 +0500

    eighth commit

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,6 @@ from app import views
 urlpatterns = [
     path('', views.job_list, name='jobs_home'),
     # path('job/1', views.job_detail), #static url
-    path('hello/', views.hello, name= 'hello'),
+    # path('hello/', views.hello, name= 'hello'),
     path('job/<int:id>', views.job_detail, name='job_detail'), #dynamic url
 ]
