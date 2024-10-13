285a861aef784bacd7b97d5390a24910a97625d2
Usman <Cap.usman21@gmail.com>
2024-10-13 23:24:19 +0500

    fixed middleware

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -12,14 +12,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
+# import dj_database_url
+# from whitenoise import WhiteNoise
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
 # Quick-start development settings - unsuitable for production
@@ -88,6 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+         
     }
 }
 
@@ -129,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = BASE_DIR / 'uploads/'
 
