7beff33ea9024def04feb4de0b0d73fb79f6b192
Usman <Cap.usman21@gmail.com>
2024-10-13 23:00:50 +0500

    clearing base_dir key error.

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -14,10 +14,12 @@ import os
 from pathlib import Path
 import django_heroku
 
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+django_heroku.settings(locals())
 
 
 # Quick-start development settings - unsuitable for production
