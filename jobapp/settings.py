55661f3bd3afa365202f770ad1ee3c2d3c7d357a
Usman <Cap.usman21@gmail.com>
2024-10-15 14:16:00 +0500

    fixed templates setting

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'jobapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
