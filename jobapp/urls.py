98c8e4a483316ee40a00cdd0ec5dd2c999033625
Usman <Cap.usman21@gmail.com>
2024-10-15 20:00:39 +0500

    changes tostatic url served

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('', include("subscribe.urls")),
     path('uploads/', include("uploadapp.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
